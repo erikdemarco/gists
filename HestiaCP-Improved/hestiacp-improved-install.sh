0fcb4540ed5fc45e84e5177ac6a8fef2786a9056
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-06-30 17:46:30 +0700

    Update hestiacp-improved-install.sh

--- HestiaCP-Improved/hestiacp-improved-install.sh
+++ HestiaCP-Improved/hestiacp-improved-install.sh
@@ -98,7 +98,7 @@ vAddString="-r $vPort -s $vHostname -e $vEmail -p $vPassword"
 curl -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
 
 #apache+nginx+phpfpm
-echo "Y" | bash hst-install.sh -a yes -n yes -w yes -o no -v no -j no -k no  -m yes -g no -x no -z no -c no -t no -i yes -b yes -q no -d no -l en -y yes $vAddString -force
+echo "Y" | bash hst-install.sh -a yes -w yes -o no -v no -j no -k no -m yes -g no -x no -z no -c no -t no -i yes -b yes -q no -d no -l en -y yes $vAddString -force
 
 
 
