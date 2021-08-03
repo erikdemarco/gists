b5e7bf8ee3cdb7c786fb9923563ea53e66b2c834
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-08-03 18:30:30 +0700

    Update caching-wordpress-rate-limited-one.sh

--- HestiaCP-Improved/tools/nginx-templates/caching-wordpress-rate-limited-one.sh
+++ HestiaCP-Improved/tools/nginx-templates/caching-wordpress-rate-limited-one.sh
@@ -13,9 +13,17 @@ str="proxy_cache_path /var/cache/nginx/$domain levels=1:2"
 str="$str keys_zone=$domain:10m inactive=60m max_size=512m;" 
 conf='/etc/nginx/conf.d/01_caching_pool.conf'
 if [ -e "$conf" ]; then
+
+    #if 'previous setting' for this domain exist, delete it first before continuing (this is to make sure if we change something it gets applied, for example if we change the levels of cache dir)
+    if [ -n "$(grep "=${domain}:" $conf)" ]; then
+        sed -i "/=${domain}:/d" $conf
+    fi
+
+    #then, if seeting for this really not exist, add it
     if [ -z "$(grep "=${domain}:" $conf)" ]; then
         echo "$str" >> $conf
     fi
+
 else
     echo "$str" >> $conf
 fi
