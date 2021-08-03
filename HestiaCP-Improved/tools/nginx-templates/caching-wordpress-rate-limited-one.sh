9be00405ac914a98eb0ec6539e3401a6c46b7cb2
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-08-03 20:17:34 +0700

    Update caching-wordpress-rate-limited-one.sh

--- HestiaCP-Improved/tools/nginx-templates/caching-wordpress-rate-limited-one.sh
+++ HestiaCP-Improved/tools/nginx-templates/caching-wordpress-rate-limited-one.sh
@@ -9,7 +9,8 @@ docroot=$5
 # https://wordpress.org/support/article/nginx/
 # https://forum.nginx.org/read.php?2,2450,273132
 # change levels to "1:2". its more common
-str="proxy_cache_path /var/cache/nginx/$domain levels=1:2" 
+# Note: #cache levels still "2", if its differs than hestiacp it will show "cache had previously different levels"
+str="proxy_cache_path /var/cache/nginx/$domain levels=2" 
 str="$str keys_zone=$domain:10m inactive=60m max_size=512m;" 
 conf='/etc/nginx/conf.d/01_caching_pool.conf'
 if [ -e "$conf" ]; then
