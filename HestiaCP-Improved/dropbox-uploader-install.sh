af03813eaca844f6e05ff31cadc4c2ae054e578c
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-11-05 12:33:03 +0700

    Update dropbox-uploader-install.sh

--- HestiaCP-Improved/dropbox-uploader-install.sh
+++ HestiaCP-Improved/dropbox-uploader-install.sh
@@ -4,3 +4,6 @@ cd dropbox
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
 chmod 755 dropbox_uploader.sh
 ./dropbox_uploader.sh
+
+#Note:
+#-) to check if its successfully installed, use: '/dropbox/dropbox_uploader.sh info'
