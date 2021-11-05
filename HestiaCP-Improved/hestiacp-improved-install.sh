6876674fc85ef25b29c8c2897ec75735860b1db9
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-11-05 12:27:53 +0700

    Update hestiacp-improved-install.sh

--- HestiaCP-Improved/hestiacp-improved-install.sh
+++ HestiaCP-Improved/hestiacp-improved-install.sh
@@ -1033,36 +1033,33 @@ fi
 if [ $vDropboxUploader == "y" ] || [ $vDropboxUploader == "Y" ]; then
   ##Automate backup to dropbox (START)
   
-  greentext "installing dropbox backup..."
-
-  #get the dropbox uploader api
-  cd /  #cd to main dir
-  mkdir dropbox
-  cd dropbox
-  curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
-  chmod 755 dropbox_uploader.sh
-  echo "$vDropboxUploaderAppKey
-  $vDropboxUploaderAppSecret
-  $vDropboxUploaderAccessToken
-  y" | ./dropbox_uploader.sh
-
-  #download the cron file (vestacp)
-  #curl -o dropbox-auto-backup-cron-hestia.sh https://gist.githubusercontent.com/erikdemarco/959e3afc29122634631e59d3e3640333/raw/f58557e0ab474eedd480e145e499de584eed6293/dropbox_auto_backup_cron.sh
-
-  #download the cron file (hestiacp)
-  curl -o dropbox-auto-backup-cron-hestia.sh https://raw.githubusercontent.com/erikdemarco/gists/main/HestiaCP-Improved/dropbox-auto-backup-cron-hestia.sh
-
-  #move the cron file for accessiblity & chmod it
-  mv dropbox-auto-backup-cron-hestia.sh ${XPANEL}bin/
-  chmod 755 ${XPANEL}bin/dropbox-auto-backup-cron-hestia.sh 
+  #only continue if dropbox-uploader already installed
+  if [ -e "/dropbox/dropbox_uploader.sh" ]
+  then
   
-  #daily cron (make backup) at 05.10 (add cron job if there is not yet added)
-  if [ -z "$(grep v-backup-users ${HESTIA}data/users/admin/cron.conf)" ]; then
-    ${XPANEL}bin/v-add-cron-job admin '10' '05' '*' '*' '*'  "sudo ${XPANEL}bin/v-backup-users"
-  fi
+    greentext "installing dropbox backup..."
+    
+    #download the cron file (vestacp)
+    #curl -o dropbox-auto-backup-cron-hestia.sh https://gist.githubusercontent.com/erikdemarco/959e3afc29122634631e59d3e3640333/raw/f58557e0ab474eedd480e145e499de584eed6293/dropbox_auto_backup_cron.sh
 
-  #daily cron (upload to dropbox) at 06.10
-  ${XPANEL}bin/v-add-cron-job admin '10' '06' '*' '*' '*'  "sudo ${XPANEL}bin/dropbox-auto-backup-cron-hestia.sh"
+    #download the cron file (hestiacp)
+    curl -o dropbox-auto-backup-cron-hestia.sh https://raw.githubusercontent.com/erikdemarco/gists/main/HestiaCP-Improved/dropbox-auto-backup-cron-hestia.sh
+
+    #move the cron file for accessiblity & chmod it
+    mv dropbox-auto-backup-cron-hestia.sh ${XPANEL}bin/
+    chmod 755 ${XPANEL}bin/dropbox-auto-backup-cron-hestia.sh 
+    
+    #daily cron (make backup) at 05.10 (add cron job if there is not yet added)
+    if [ -z "$(grep v-backup-users ${HESTIA}data/users/admin/cron.conf)" ]; then
+        ${XPANEL}bin/v-add-cron-job admin '10' '05' '*' '*' '*'  "sudo ${XPANEL}bin/v-backup-users"
+    fi
+
+    #daily cron (upload to dropbox) at 06.10
+    ${XPANEL}bin/v-add-cron-job admin '10' '06' '*' '*' '*'  "sudo ${XPANEL}bin/dropbox-auto-backup-cron-hestia.sh"
+    
+  else
+    redtext "Error: dropbox-uploader is not installed yet!"
+  fi
 
   ##Automate backup to dropbox (END)
 fi
