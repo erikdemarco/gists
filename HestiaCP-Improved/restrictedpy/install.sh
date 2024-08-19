a64f7cdcf82ed12b0fe7339feb516458c698fded
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2024-08-19 11:45:36 +0700

    Update install.sh

--- HestiaCP-Improved/restrictedpy/install.sh
+++ HestiaCP-Improved/restrictedpy/install.sh
@@ -1,5 +1,5 @@
 mkdir /tempdocker && cd /tempdocker
 curl -O https://raw.githubusercontent.com/erikdemarco/gists/main/HestiaCP-Improved/restrictedpy/Dockerfile
 sudo docker build --tag app-restrictedpy .
-docker run -d --memory=256m --cpus="1" --restart always --name app-restrictedpy -p 127.0.0.1:5001:5001 app-restrictedpy
+docker run -d --memory=256m --restart always --name app-restrictedpy -p 127.0.0.1:5001:5001 app-restrictedpy
 cd / && rm -r /tempdocker
