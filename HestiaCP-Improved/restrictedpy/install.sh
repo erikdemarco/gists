759076bd34da24cefda39bdac19addb967422abc
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2024-08-19 10:48:46 +0700

    Update install.sh

--- HestiaCP-Improved/restrictedpy/install.sh
+++ HestiaCP-Improved/restrictedpy/install.sh
@@ -1,5 +1,5 @@
 mkdir /tempdocker && cd /tempdocker
 curl -O https://raw.githubusercontent.com/erikdemarco/gists/main/HestiaCP-Improved/restrictedpy/Dockerfile
 sudo docker build --tag flask-app .
-docker run -d --restart always --name flask-app -p 127.0.0.1:5001:5001 flask-app
+docker run -d --memory=256m --cpus="1" --restart always --name flask-app -p 127.0.0.1:5001:5001 flask-app
 cd / && rm -r /tempdocker
